910eef6818fe52e826adcb28d7f64d8071264b3a
Diallo Mamadou Lamarana <lyve.diallo@gmail.com>
2019-12-31 19:43:23 +0000

    add Header Component, AddTodo feature

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
 import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+import Header from './components/layout/Header';
+import uuid from 'uuid';
 
 class App extends Component {
 	state = {
 		todos: [
 			{
-				id: 1,
+				id: uuid.v4(),
 				title: 'First Tastk',
 				completed: false
 			},
 			{
-				id: 2,
+				id: uuid.v4(),
 				title: 'Second Tastk',
-				completed: true
+				completed: false
 			},
 			{
-				id: 3,
+				id: uuid.v4(),
 				title: 'Third Tastk',
 				completed: false
 			}
@@ -42,10 +45,22 @@ class App extends Component {
 		});
 	};
 
+	//Add TodoItem
+	addTodo = (title) => {
+		const newTodo = { id: uuid.v4(), title: title, completed: false };
+		this.setState({
+			todos: [ ...this.state.todos, newTodo ]
+		});
+	};
+
 	render() {
 		return (
 			<div className="App">
-				<Todos todos={this.state.todos} markComplete={this.markComplete} deleteTodo={this.deleteTodo} />
+				<div className="container">
+					<Header />
+					<AddTodo addTodo={this.addTodo} />
+					<Todos todos={this.state.todos} markComplete={this.markComplete} deleteTodo={this.deleteTodo} />
+				</div>
 			</div>
 		);
 	}
